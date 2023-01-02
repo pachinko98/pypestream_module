91082120d8ec76b579eedb6d7420de5286f53c8a
Jason Euceda <75599223+pachinko98@users.noreply.github.com>
2023-01-02 11:38:33 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+#get interaction data from a date range (longer than 24 hours)
+
+#get metadata_gateway info formatted
+
+#get specific piece of data from a specific date
+
